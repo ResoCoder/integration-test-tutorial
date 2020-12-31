77b3c597e2c56115bfb76db170943386f9b0773f
Matt <mattresetar@resocoder.com>
2020-12-31 08:35:44 +0100

    Finished project fix

--- android/app/src/androidTest/java/com/resocoder/integration_test_tutorial/MainActivityTest.java
+++ android/app/src/androidTest/java/com/resocoder/integration_test_tutorial/MainActivityTest.java
@@ -1,12 +1,12 @@
 // Change this line to YOUR package name:
-package com.resocoder.integration_testing_prep;
+package com.resocoder.integration_test_tutorial;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
-import com.resocoder.integration_testing_prep.MainActivity;
+import com.resocoder.integration_test_tutorial.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
